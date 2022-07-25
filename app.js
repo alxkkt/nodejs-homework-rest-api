80fa2aa303a0f1e6d19ec9bf26a9577bcccf14d5
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-07-25 16:14:37 +0200

    added basic files for hw

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs/promises");
+const path = require("path");
 
 require("dotenv").config();
+const tempDir = path.join(__dirname, "temp");
 
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/users");
@@ -14,6 +18,18 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
